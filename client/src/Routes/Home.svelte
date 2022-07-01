a503c8eed4ebdf7953c0be93e8762df4c558910c
Manas Hejmadi <manas2all@outlook.com>
2022-07-01 14:25:22 +0530

    Test

--- client/src/Routes/Home.svelte
+++ client/src/Routes/Home.svelte
@@ -1,4 +1,10 @@
 <div class="p-4">
     <h1 class="text-6xl text-green-700">HOME PAGE</h1>
-    <p class="text-4xl">THis is a text</p>
+    <p class="text-2xl">THis is a text</p>
 </div>
+
+<style lang='scss'>
+    p{
+        color: red;
+    }
+</style>
